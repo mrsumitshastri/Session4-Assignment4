a285176b08d2fd232be07c3b301627698784c2b8
Sumit Shastri <mrsumitshastri@gmail.com>
2016-03-13 14:08:40 +0530

    Commit
    Commit

--- src/com/Session4/Assignment4/ReverseStringExample.java
+++ src/com/Session4/Assignment4/ReverseStringExample.java
@@ -4,11 +4,16 @@ import java.util.Scanner;
 
 public class ReverseStringExample {
 
+	/*
+	 * Print reverse of a given String value.
+	 * 
+	 */
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter a string");
 		String str = scan.next();
 		
+		System.out.print("Reverse String is: ");
 		for (int i= str.length()-1; i >=0; i--)
 		{
 			System.out.print(str.charAt(i));
